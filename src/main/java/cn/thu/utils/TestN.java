f63f15c807c98194b29e49318a0dde09b86fd498
tianhu9102 <tianhu9102@users.noreply.github.com>
2018-12-10 16:01:40 +0800

    Update TestN.java

--- src/main/java/cn/thu/utils/TestN.java
+++ src/main/java/cn/thu/utils/TestN.java
@@ -13,24 +13,99 @@ public class TestN
 
     public static void main(String[] args)
     {
-
+        compute(testDatas().get(0), testDatas().get(1), testDatas().get(2));
     }
 
     // 返回三个视图中互相最近点的列表
-    public List<List<Point>> compute(List<Point> lstPoints_Red, List<Point> lstPoints_Dark, List<Point> lstPoints_Green)
+    public static List<List<Point>> compute(List<Point> lstPoints_Red, List<Point> lstPoints_Dark,
+                    List<Point> lstPoints_Green)
     {
+
+        // 返回结果
+        List<List<Point>> lstPoints = new ArrayList<List<Point>>();
+
         // 1.计算红黑点云互相最近点的列表
-        List<List<Point>> computeMupullyRD = computeMupully(lstPoints_Red, lstPoints_Dark);
+        List<List<Point>> mupullyList1 = computeMupully(lstPoints_Red, lstPoints_Dark);
 
         // 2.计算红绿点云互相最近点的列表
-        List<List<Point>> computeMupullyRG = computeMupully(lstPoints_Red, lstPoints_Green);
+        List<List<Point>> mupullyList2 = computeMupully(lstPoints_Red, lstPoints_Green);
 
         // 3.计算黑绿点云互相最近点的列表
-        List<List<Point>> computeMupullyDG = computeMupully(lstPoints_Dark, lstPoints_Green);
+        List<List<Point>> mupullyList3 = computeMupully(lstPoints_Dark, lstPoints_Green);
+
+        // 筛选条件
+        for (int i = 0; i < mupullyList1.size(); i++)
+        {
+            List<Point> tmpPointLst = mupullyList1.get(i);
+
+            if (tmpPointLst.size() == 2)
+            {
+                // 第一种情况，在三个视图都存在
+                Point pointInView1 = tmpPointLst.get(0);
+                Point pointInView2 = tmpPointLst.get(1);
+                Point pointInView3 = existIn(pointInView1, mupullyList3);
+                if (null != pointInView3)
+                {
+                    Point pointInView3_mirror_View2 = existIn(pointInView3, mupullyList2);
+                    if (null != pointInView3_mirror_View2)
+                    {
+                        if (pointsEquals(pointInView3_mirror_View2, pointInView3))
+                        {
+                            List<Point> lstMupullyPoints = new ArrayList<Point>();
+                            lstMupullyPoints.add(pointInView1);
+                            lstMupullyPoints.add(pointInView2);
+                            lstMupullyPoints.add(pointInView3);
+
+                            // 返回结果lstPoints中添加
+                            lstPoints.add(lstMupullyPoints);
+                        }
+                    }
+                }
+
+                // TODO::第二种情况，在两个视图存在
+
+            }
+
+        }
+
+        return lstPoints;
+    }
+
+    public static Point existIn(Point point, List<List<Point>> mupullyList)
+    {
+        Point anotherPoint = new Point();
+
+        for (int i = 0; i < mupullyList.size(); i++)
+        {
+            List<Point> lstPoint = mupullyList.get(i);
+
+            for (int j = 0; j < lstPoint.size(); j++)
+            {
+                Point pt = lstPoint.get(j);
+
+                if (pointsEquals(pt, point))
+                {
+                    anotherPoint = pt;
+                    return anotherPoint;
+                }
+
+            }
+
+        }
 
         return null;
     }
 
+    public static boolean pointsEquals(Point p0, Point p1)
+    {
+        if (p0.getX() == p1.getX() && p0.getY() == p1.getY() && p0.getZ() == p1.getZ())
+        {
+            return true;
+        }
+        return false;
+    }
+
+    // 求两个视图互相最近点的列表
     public static List<List<Point>> computeMupully(List<Point> lstPoints0, List<Point> lstPoints1)
     {
 
@@ -45,10 +120,10 @@ public class TestN
             Point pointView0_mirror = findNearestPoint(pointView1, lstPoints1);
 
             // 互相最近
-            lstMupully.add(pointView0);
+            lstMupully.add(pointView0); // 初始化，添加一个点
             if (pointView0_mirror == pointView0)
             {
-                lstMupully.add(pointView1);
+                lstMupully.add(pointView1); // 满足条件，添加另外一个点（另一个视图）
             }
 
             data.add(lstMupully);
@@ -104,10 +179,11 @@ public class TestN
     }
 
     // 测试1：构造模拟数据： 点云（红色）、点云（绿色）、点云（黑色）
-    public static List<Point> testDatas()
+    public static List<List<Point>> testDatas()
     {
-        List<Point> lstPoints_Red = new ArrayList<Point>();
+        List<List<Point>> lst = new ArrayList<List<Point>>();
 
+        List<Point> lstPoints_Red = new ArrayList<Point>();
         for (int i = 0; i < 20; i++)
         {
 
@@ -123,7 +199,42 @@ public class TestN
             lstPoints_Red.add(point);
         }
 
-        return lstPoints_Red;
+        List<Point> lstPoints_Green = new ArrayList<Point>();
+        for (int i = 0; i < 20; i++)
+        {
+
+            double random_x = new Random().nextDouble();// 0.0-1.0的随机数
+            double random_y = new Random().nextDouble();// 0.0-1.0的随机数
+            double random_z = new Random().nextDouble();// 0.0-1.0的随机数
+
+            Point point = new Point();
+            point.setX(random_x * 5);
+            point.setY(random_y * 14);
+            point.setZ(random_z * 6);
+
+            lstPoints_Green.add(point);
+        }
+
+        List<Point> lstPoints_Dark = new ArrayList<Point>();
+        for (int i = 0; i < 20; i++)
+        {
+
+            double random_x = new Random().nextDouble();// 0.0-1.0的随机数
+            double random_y = new Random().nextDouble();// 0.0-1.0的随机数
+            double random_z = new Random().nextDouble();// 0.0-1.0的随机数
+
+            Point point = new Point();
+            point.setX(random_x * 64);
+            point.setY(random_y * 24);
+            point.setZ(random_z * 15);
+
+            lstPoints_Dark.add(point);
+        }
+
+        lst.add(lstPoints_Red);
+        lst.add(lstPoints_Dark);
+        lst.add(lstPoints_Green);
+        return lst;
     }
 
     public static void testNearestPoint()
